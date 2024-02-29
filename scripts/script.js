d6175b70b31181df029ea6b3590700261649e5da
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-28 21:27:54 -0800

    added a compleate phone hunting web

--- scripts/script.js
+++ scripts/script.js
@@ -28,15 +28,51 @@ const displayPhones = (phones, isShowAll) =>{
         phoneCard.innerHTML = `<figure><img src=${phone.image} alt="Shoes" /></figure>
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phone.slug}') "
+             class="btn btn-primary">See More Details</button>
           </div>
         </div>`
         phoneContainer.appendChild(phoneCard);
     });
     toggleLoadingSpinner(false);
 }
+
+const handleShowDetail = async(id) =>{
+    console.log('clicked show details', id);
+    const res = await fetch( `https://openapi.programming-hero.com/api/phone/${id}`);
+    
+    const data = await res.json();
+    console.log(data)
+    const phone =data.data;
+    showPhoneDetails(phone);
+    
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+    const phoneName = document.getElementById('show_phone_name')
+    phoneName.innerText =  phone.name;
+
+    const showDetailsContainer = document.getElementById('phoneDetailsContainer');
+    showDetailsContainer.innerHTML= `
+    <img class="text-center" src="${phone.image}" alt=""/>
+     <p><span class="text-xl font-semibold">Storage: </span>${phone?.mainFeatures.storage}</p>
+     <p><span class="text-xl font-semibold">Display Size: </span>${phone?.mainFeatures.displaySize}</p>
+     <p><span class="text-xl font-semibold">Chipset: </span>${phone?.mainFeatures.chipSet}</p>
+     <p><span class="text-xl font-semibold">Memory: </span>${phone?.mainFeatures.memory}</p>
+     <p><span class="text-xl font-semibold">Slug: </span>${phone?.slug}</p>
+     <p><span class="text-xl font-semibold">Release data: </span>${phone?.releaseDate
+     }</p>
+     <p><span class="text-xl font-semibold">Brand: </span>${phone?.brand}</p>
+
+    `
+    
+
+
+    show_details_modal.showModal()
+}
+
 const handleSearch = (isShowAll)  =>{
     toggleLoadingSpinner(true);
     const searchField = document.getElementById("search-filed");
